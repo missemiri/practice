4be0ec88bde32db28af98b33dfd3ca0850d86641
miss.em6513@gmail.com <emirimorita@Emiris-MacBook-Air.local>
2020-04-06 14:15:38 +0900

    adding new file practicefile.py

---
+++ practicefile.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr  6 14:12:32 2020
+
+@author: emirimorita
+"""
+
+## this is my practice file
+print("This is my practice file")
